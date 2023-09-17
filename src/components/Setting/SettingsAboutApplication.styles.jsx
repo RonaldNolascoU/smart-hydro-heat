ba3347649175eea2ef4ba4f1b13eb1251ab626a9
Victor-Rodriguez <viictor1275@gmail.com>
2023-09-17 17:17:33 -0600

    victor/screen-settings

---
+++ src/components/Setting/SettingsAboutApplication.styles.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    contenedorImage: {
+        padding: 15,
+        alignItems: 'center'
+    },
+    contenedorDescripcion: {
+        padding: 10,
+    },
+    textDescripcion: {
+        color: '#68708a',
+        textAlign: 'justify'
+    }
+}) 
---
+++ src/components/Setting/SettingsChangePassword.styles.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    contenedorText: {
+        flexDirection: 'row',
+        width: '100%',
+        padding: 5,
+        //backgroundColor: 'red'
+        //alignItems: 'center',
+    },
+    contenedorInputs: {
+        flex: 3,
+        padding: 3,
+        alignItems: 'center',
+        //backgroundColor: 'purple'
+    },
+    contenedorIcon: {
+        width: 30,
+        padding: 3,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    contenedorButton: {
+        alignItems: 'center'
+    },
+    textInput: {
+        width: '95%',
+        marginTop: '3%',
+        marginBottom: '3%'
+    },
+    buttonPassword: {
+        marginTop: 15,
+        width: '75%',
+        backgroundColor: '#68708a'
+    },
+    iconoNuevaContra: {
+        marginBottom: 30
+    }
+})
