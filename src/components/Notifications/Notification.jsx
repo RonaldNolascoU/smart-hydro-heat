d1ed51961e9a89e7ce68da0625fa20f14bdbe4f8
Karletty <karletty.carolina@gmail.com>
2023-09-29 14:14:07 -0600

    array de prueba para notificaciones

--- src/components/Notifications/Notification.jsx
+++ src/components/Notifications/Notification.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./Notification.styles";
+import notificationExample from "./notificationTest"
 
 export function Notifications() {
-    const [notifications, setNotifications] = useState([]);
+    const [notifications, setNotifications] = useState(notificationExample);
     return (
         <View>
             {notifications.map((notification, i) =>
---
+++ src/components/Notifications/notificationTest.js
@@ -0,0 +1,26 @@
+export const testNotifications = [
+    {
+         date: new Date(2023, 0, 17),
+         hour: "8:00 PM",
+         title: "Warning heater still on",
+         description: "Heater still is on 2 hours ago",
+    },
+    {
+         date: new Date(2023, 0, 17),
+         hour: "6:00 PM",
+         title: '"Night bath" alarm',
+         description: "Heater on to 30°C by alarm",
+    },
+    {
+         date: new Date(2023, 0, 16),
+         hour: "10:00 AM",
+         title: "Heather off",
+         description: "Desactivated manually",
+    },
+    {
+         date: new Date(2023, 0, 16),
+         hour: "9:00 AM",
+         title: "Heather on",
+         description: "Activated manually on 30°C",
+    },
+]
